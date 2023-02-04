9d46792aefef5439eb6c49ab07adfd32fc145df3
Habidhossen <habidhossen2@gmail.com>
2023-02-05 01:32:04 +0600

    updated

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 const app = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Drive Code
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zwgebdo.mongodb.net/?retryWrites=true&w=majority`;
+// mongoDB drive code
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3mjkw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
